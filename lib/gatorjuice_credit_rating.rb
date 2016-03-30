f2d0176d0611b85e98c69857bc696b13c3d568aa
Jamie <gatorjuice@gmail.com>
2016-03-30 18:10:02 -0500

    added range test

--- lib/gatorjuice_credit_rating.rb
+++ lib/gatorjuice_credit_rating.rb
@@ -1,16 +1,26 @@
 require "gatorjuice_credit_rating/version"
+require "unirest"
 
 module GatorjuiceCreditRating
   class Assessment
-    def self.new(options_hash)
-      {
-        "propensity": 0.26532,
-        "ranking": "C"
-      }
+    def initialize(input_hash)
+      @propensity = input_hash["propensity"].to_f
+      @ranking = input_hash["ranking"]
     end
 
-    def self.test
-      5
+    def self.inquiry(input_options)
+      if input_options[:age] && input_options[:income] && input_options[:zipcode]
+        response = Unirest.get("http://jsonplaceholder.typicode.com/users?income=#{input_options[:income]}&age=#{input_options[:age]}&zipcode=#{input_options[:zipcode]}").body
+        # assuming this was the actual endpoint, I'd get this response back
+        response = {
+          "propensity" => rand.round(5),
+          "ranking" => %w{A B C D E F}.sample
+        }
+        assessment = Assessment.new(response)
+        return assessment
+      else
+        return "invalid inquiry"
+      end
     end
   end
 end
--- spec/gatorjuice_credit_rating_spec.rb
+++ spec/gatorjuice_credit_rating_spec.rb
@@ -6,20 +6,18 @@ describe GatorjuiceCreditRating do
   end
 
   describe GatorjuiceCreditRating::Assessment do
-    describe '#self.new' do
-      it 'returns a test JSON response' do
-        expect(GatorjuiceCreditRating::Assessment.new(income: 50000, zip: 60625, age: 35)).to eq({"propensity": 0.26532, "ranking": "C"})
+    describe '#self.inquiry' do
+      it 'returns a ruby Assessment Object' do
+        expect(GatorjuiceCreditRating::Assessment.inquiry(income: 50000, zipcode: 60625, age: 35)).to be_a GatorjuiceCreditRating::Assessment
+      end
+
+      it 'alerts the user if params were insufficient' do
+        expect(GatorjuiceCreditRating::Assessment.inquiry(income: 50000)).to include("invalid inquiry")
+      end
+
+      it 'expects a propensity between 0 and 1' do
+        expect(GatorjuiceCreditRating::Assessment.inquiry(income: 50000, zipcode: 60625, age: 35).propensity).to be_between(0, 1).exclusive
       end
     end
   end
 end
-
-
-
-  # it 'returns a test JSON response' do
-  #   expect(GatorjuiceCreditRating::Assessment.new(
-  #     income: 50000,
-  #     zip: 60625,
-  #     age: 35
-  #   ).to eq({"propensity" => 0.26532, "ranking" => "C"}) 
-  # end
