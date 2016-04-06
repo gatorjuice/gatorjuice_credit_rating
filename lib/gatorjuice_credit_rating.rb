cfc5e9ca109260361bc692b294a841163e032c17
Jamie <gatorjuice@gmail.com>
2016-04-06 16:26:16 -0500

    added actual api endpoint updated version number

--- lib/gatorjuice_credit_rating.rb
+++ lib/gatorjuice_credit_rating.rb
@@ -12,16 +12,15 @@ module GatorjuiceCreditRating
 
     def self.inquiry(input_options)
       if input_options[:age] && input_options[:income] && input_options[:zipcode] && input_options.class == Hash
-        response = Unirest.get("http://jsonplaceholder.typicode.com/users?income=#{input_options[:income]}&age=#{input_options[:age]}&zipcode=#{input_options[:zipcode]}&api_token=ENV[API_TOKEN]").body
-        # assuming this was the actual endpoint, I'd get this response back
-        response = {
-          "propensity" => rand.round(5),
-          "ranking" => %w(A B C D E F).sample
-        }
+        response = Unirest.post(
+          "https://pacific-stream-61271.herokuapp.com/api/v1/inquiries?income=#{input_options[:income]}&age=#{input_options[:age]}&zipcode=#{input_options[:zipcode]}&api_token=ABCDEFG123456"
+        ).body
         @assessment = Assessment.new(response)
         return @assessment
       else
-        raise ArgumentError.new("#inquiry takes one hash as an argument and must have keys and values for: age, income, zipcode")
+        raise ArgumentError.new(
+          "#inquiry takes one hash as an argument and must have keys and values for: age, income, zipcode"
+        )
       end
     end
   end
