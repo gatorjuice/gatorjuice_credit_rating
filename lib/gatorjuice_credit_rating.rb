97ce9c829f26f828a9f1c5bfb83b326609674cc6
Jamie <gatorjuice@gmail.com>
2016-04-06 20:04:08 -0500

    added server response tests

--- lib/gatorjuice_credit_rating.rb
+++ lib/gatorjuice_credit_rating.rb
@@ -13,14 +13,25 @@ module GatorjuiceCreditRating
     def self.inquiry(input_options)
       if input_options[:age] && input_options[:income] && input_options[:zipcode] && input_options.class == Hash
         response = Unirest.post(
-          "https://pacific-stream-61271.herokuapp.com/api/v1/inquiries?income=#{input_options[:income]}&age=#{input_options[:age]}&zipcode=#{input_options[:zipcode]}&api_token=ABCDEFG123456"
-        ).body
-        @assessment = Assessment.new(response)
-        return @assessment
+          "https://pacific-stream-61271.herokuapp.com/api/v1/inquiries",
+          headers: {
+            "Accept" => "application/json"
+          },
+          parameters: {
+            age: input_options[:age].to_s,
+            income: input_options[:income].to_s,
+            zipcode: input_options[:zipcode],
+            api_token: "ABCDEFG1234567" # ENV[API_TOKEN]
+          }
+        ).body 
+        if !response["response"]
+          @assessment = Assessment.new(response)
+          return @assessment
+        else
+          return response["response"]
+        end
       else
-        raise ArgumentError.new(
-          "#inquiry takes one hash as an argument and must have keys and values for: age, income, zipcode"
-        )
+        raise ArgumentError, "#inquiry takes one hash as an argument and must have keys and values for: age, income, zipcode"
       end
     end
   end
--- spec/gatorjuice_credit_rating_spec.rb
+++ spec/gatorjuice_credit_rating_spec.rb
@@ -1,28 +1,96 @@
 require 'spec_helper'
+require 'unirest'
+
+GJCRA = GatorjuiceCreditRating::Assessment
 
 describe GatorjuiceCreditRating do
   it 'has a version number' do
     expect(GatorjuiceCreditRating::VERSION).not_to be nil
   end
 
-  describe GatorjuiceCreditRating::Assessment do
+  describe GJCRA do
     describe '#self.inquiry' do
       it 'returns a ruby Assessment Object' do
-        expect(GatorjuiceCreditRating::Assessment.inquiry(income: 50000, zipcode: 60625, age: 35)).to be_a GatorjuiceCreditRating::Assessment
+        expect(
+          GJCRA.inquiry(
+            income: 50000, 
+            zipcode: 60625, 
+            age: 35
+          )
+        ).to be_a GJCRA
       end
 
       it 'raises argument error if parameters are insuffient' do
-        expect {
-          GatorjuiceCreditRating::Assessment.inquiry(income: 50000)
-        }.to raise_error(ArgumentError)
+        expect { GJCRA.inquiry(income: 50000) }.to raise_error(ArgumentError)
       end
 
       it 'expects a propensity between 0 and 1' do
-        expect(GatorjuiceCreditRating::Assessment.inquiry(income: 50000, zipcode: 60625, age: 35).propensity).to be_between(0, 1).exclusive
+        inquiry = GJCRA.inquiry(
+          income: 50000,
+          zipcode: 60625, 
+          age: 35
+        )
+        expect(inquiry.propensity).to be_between(0, 1).exclusive
       end
 
       it "expects a ranking between 'A' and 'F'" do
-        expect(%w(A B C D E F)).to include(GatorjuiceCreditRating::Assessment.inquiry(income: 50000, zipcode: 60625, age: 35).ranking)
+        inquiry = GJCRA.inquiry(
+          income: 50000,
+          zipcode: 60625, 
+          age: 35
+        )
+        expect(%w(A B C D E F)).to include(
+          GJCRA.inquiry(
+            income: 50000, 
+            zipcode: 60625, 
+            age: 35
+          ).ranking
+        )
+      end
+
+      it 'sends a message back if the server doesn\'t detect an age parameter' do
+        response = Unirest.post(
+          "https://pacific-stream-61271.herokuapp.com/api/v1/inquiries",
+          headers: {
+            "Accept" => "application/json"
+          },
+          parameters: {
+            income: "32342",
+            zipcode: "60625",
+            api_token: "ABCDEFG1234567" # ENV[API_TOKEN]
+          }
+        ).body 
+        expect(response).to eq({"response" => "missing age parameter"})
+      end
+
+      it 'sends a message back if the server doesn\'t detect an income parameter' do
+        response = Unirest.post(
+          "https://pacific-stream-61271.herokuapp.com/api/v1/inquiries",
+          headers: {
+            "Accept" => "application/json"
+          },
+          parameters: {
+            age: "35",
+            zipcode: "60625",
+            api_token: "ABCDEFG1234567" # ENV[API_TOKEN]
+          }
+        ).body 
+        expect(response).to eq({"response" => "missing income parameter"})
+      end
+
+      it 'sends a message back if the server doesn\'t detect a zipcode parameter' do
+        response = Unirest.post(
+          "https://pacific-stream-61271.herokuapp.com/api/v1/inquiries",
+          headers: {
+            "Accept" => "application/json"
+          },
+          parameters: {
+            age: "35",
+            income: "32342",
+            api_token: "ABCDEFG1234567" # ENV[API_TOKEN]
+          }
+        ).body 
+        expect(response).to eq({"response" => "missing zipcode parameter"})
       end
     end
   end
