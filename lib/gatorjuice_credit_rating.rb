a1c1b7970f013c83637e0f50ae1a606f338234e1
Jamie <gatorjuice@gmail.com>
2016-04-07 15:49:07 -0500

    refactored inquiry method a little

--- lib/gatorjuice_credit_rating.rb
+++ lib/gatorjuice_credit_rating.rb
@@ -11,26 +11,27 @@ module GatorjuiceCreditRating
     end
 
     def self.inquiry(input_options)
-      if 
-        input_options[:age] && 
-        input_options[:age].to_i >= 18 && 
-        !!Integer(input_options[:age]) && 
-        input_options[:income] && 
-        input_options[:income].to_i > 0 && 
-        !!Integer(input_options[:income]) &&
-        input_options[:zipcode] && 
-        input_options[:zipcode].to_s.length == 5 && 
-        !!Integer(input_options[:zipcode]) && 
-        input_options.class == Hash
+      age = input_options[:age].to_s
+      income = input_options[:income].to_s
+      zipcode = input_options[:zipcode].to_s
+
+      if age && income && zipcode &&
+         age.to_i >= 18 && 
+         income.to_i > 0 && 
+         zipcode.length == 5 && 
+         !!Integer(age) && 
+         !!Integer(income) &&
+         !!Integer(zipcode) && 
+         input_options.class == Hash
         response = Unirest.get(
           "https://pacific-stream-61271.herokuapp.com/api/v1/inquiries",
           headers: {
             "Accept" => "application/json"
           },
           parameters: {
-            age: input_options[:age].to_s,
-            income: input_options[:income].to_s,
-            zipcode: input_options[:zipcode],
+            age: age,
+            income: income,
+            zipcode: zipcode,
             api_token: "ABCDEFG1234567" # ENV[API_TOKEN]
           }
         ).body 
